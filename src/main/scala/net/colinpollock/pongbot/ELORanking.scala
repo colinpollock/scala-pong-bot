be3bb0e994ad76bbf92762f8355927afdb3dbfbf
Colin Pollock <colin@wordnik.com>
2012-06-10 20:54:42 -0700

    Updating ELO rating code.

--- src/main/scala/net/colinpollock/pongbot/ELORanking.scala
+++ src/main/scala/net/colinpollock/pongbot/ELORanking.scala
@@ -1,11 +1,46 @@
 package net.colinpollock.pongbot
 
+//TODO: change filename
 
-object ELORanking {
+object ELORating {
 
-  def calculate(rankA: Int, rankB: Int): (Int, Int) =
-    (calcOneRank(rankA, rankB), calcOneRank(rankB, rankA))
+//  def calculate(rankA: Double, rankB: Double): (Double, Double) =
+//    (calcOneRank(rankA, rankB), calcOneRank(rankB, rankA))
 
-  private def calcOneRank(a: Int, b: Int): Int =
-    1 / (1 + math.pow(10, (a - b) / 400.0))
+
+  private def kValueFromRating(rating: Double): Int = rating match {
+    case r if r > 2400 => 16
+    case r if r >= 2100 => 24
+    case _ => 32
+  }
+    
+
+  def titleFromRating(rating: Double): String = rating match {
+    case r if r >= 2400 => "Super Grandmaster"
+    case r if r >= 2200 => "Grandmaster"
+    case r if r >= 1800 => "Master"
+    case r if r >= 1600 => "Expert"
+    case r if r >= 1400 => "Class D"
+    case r if r >= 1200 => "Class C"
+    case r if r >= 1000 => "Class B"
+    case r if r >= 800 => "Class A"
+    case _ => "Novice"
+  }
+
+
+  def updated(winnerRating: Int, loserRating: Int): (Int, Int) = {
+    val loserExpectation = expectation(loserRating, winnerRating)
+    val k = kValueFromRating(winnerRating)
+    val newWinnerRating = 
+      math.round(winnerRating + (k * (loserExpectation))).toInt
+    val newLoserRating = loserRating - newWinnerRating + winnerRating
+    (newWinnerRating, newLoserRating)
+  }
+
+   /* 
+    * Return the expected probability that a player with rating `a` will beat
+    * a player with rating `b`.
+   */
+  private def expectation(a: Int, b: Int): Double = 
+    1. / (1 + math.pow(10, (b.toDouble - a.toDouble) / 400.0))
 }
