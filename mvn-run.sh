a1c47aae3f28b3871909891fed829bc2d111d2c2
Colin Pollock <colin@wordnik.com>
2012-06-10 20:57:31 -0700

    Small change to run script.

--- mvn-run.sh
+++ mvn-run.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-echo "" > classpath.txt
+echo "" > .classpath.txt
 for file in `ls target`;
-        do echo -n 'target/' >> classpath.txt;
-        echo -n $file >> classpath.txt;
-        echo -n ':' >> classpath.txt;
+        do echo -n 'target/' >> .classpath.txt;
+        echo -n $file >> .classpath.txt;
+        echo -n ':' >> .classpath.txt;
 done
 for file in `ls target/lib`;
-	do echo -n 'target/lib/' >> classpath.txt;
-	echo -n $file >> classpath.txt;
-	echo -n ':' >> classpath.txt;
+	do echo -n 'target/lib/' >> .classpath.txt;
+	echo -n $file >> .classpath.txt;
+	echo -n ':' >> .classpath.txt;
 done
-export CLASSPATH=$(cat classpath.txt)
+export CLASSPATH=$(cat .classpath.txt)
 
 export JAVA_CONFIG_OPTIONS="-Xms1000m -Xmx4g -XX:NewSize=256m -XX:MaxNewSize=256m -XX:+UseConcMarkSweepGC -XX:+UseParNewGC -XX:PermSize=256m -XX:MaxPermSize=128m"
 
